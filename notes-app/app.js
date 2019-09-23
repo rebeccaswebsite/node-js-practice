9bdb1b20c5a86126a2430b3f109ec4c3349dd5d2
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-09-23 18:58:29 +0100

    First commit: importing node modules practice

---
+++ notes-app/app.js
@@ -0,0 +1,9 @@
+// const fs = require("fs")
+
+// fs.writeFileSync("notes.txt", "My name is Rebecca")
+
+// fs.appendFileSync("notes.txt", "  Hi I'm appended")
+
+const name = require("./utils.js")
+
+console.log(name)
