1bc472600bb528a816ed3268bed7408d2a162a70
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-09-23 19:28:41 +0100

    Importing npm modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,9 @@
+const validator = require("validator")
 const getNotes = require("./notes.js")
 
 const notes = getNotes()
 
 console.log(notes)
+
+// console.log(validator.isEmail("rebecca@example.com"))
+console.log(validator.isURL("wwww.example.com"))
