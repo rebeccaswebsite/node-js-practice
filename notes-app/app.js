68a87cc21398e7d2fe240ac23c92a9a8f70081f5
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-09-25 16:07:02 +0100

    Using npm modules to print in colour

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,9 @@
-const validator = require("validator")
+const chalk = require("chalk")
+
 const getNotes = require("./notes.js")
 
 const notes = getNotes()
 
 console.log(notes)
 
-// console.log(validator.isEmail("rebecca@example.com"))
-console.log(validator.isURL("wwww.example.com"))
+console.log(chalk.green("Success!"))
