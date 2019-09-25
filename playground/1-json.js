f7b453ec3490b80d6da973c537e6d4d391fc5325
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-09-25 17:33:15 +0100

    Practice with JSON

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,12 +1,24 @@
-const book = {
-  title: "Ego is the Enemy",
-  author: "Ryan Holiday"
-}
+const fs = require("fs")
+// const book = {
+//  title: "Ego is the Enemy",
+//   author: "Ryan Holiday"
+// }
 
-// Turn JavaScript object into JSON
-const bookJSON = JSON.stringify(book)
-console.log(bookJSON)
+// // Turn JavaScript object into JSON
+// const bookJSON = JSON.stringify(book)
+// fs.writeFileSync("1-json.json", bookJSON)
 
-// Convert JSON back to original JS object
-const parsedData = JSON.parse(bookJSON)
-console.log(parsedData)
+// // Convert JSON back to original JS object
+// const parsedData = JSON.parse(bookJSON)
+// console.log(parsedData)
+
+const dataBuffer = fs.readFileSync("./1-json.json")
+const dataJSON = dataBuffer.toString()
+const user = JSON.parse(dataJSON)
+
+user.name = "Rebecca"
+user.age = 24
+
+const userJSON = JSON.stringify(user)
+
+fs.writeFileSync("1-json.json", userJSON)
